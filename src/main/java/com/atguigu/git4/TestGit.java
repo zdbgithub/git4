ad67d241c9d62210255d5e71a3b4f1225f72329e
zhangdaben <421164490@qq.com>
2017-08-03 17:31:27 +0800

    zdb commit again too

--- src/main/java/com/atguigu/git4/TestGit.java
+++ src/main/java/com/atguigu/git4/TestGit.java
@@ -3,5 +3,6 @@ package com.atguigu.git4;
 public class TestGit {
 	public static void main(String[] args) {
 		System.out.println("zdb commit again");
+		System.out.println("zdb commit again too");
 	}
 }
